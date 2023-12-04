ffeaaf39996b16e0ce33e7317b68619b4c9863a9
nurfadilahzulfi <nurfadilahzulfi@gmail.com>
2023-12-04 22:47:46 +0700

    dasdas

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.118.2">
-    <title>DeCafe - Aplikasi Pemesanan Makanan dan Minuman Cafe</title>
+    <title>Laundry-Bersih</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
